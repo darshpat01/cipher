3072d3a2715e7fd07f14bab8c60916e40bcafd8a
darshan <darshpat01@gmail.com>
2023-03-27 15:20:15 +0530

    auth done

---
+++ Models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  interests: {
+    type: Array,
+    Enum: [
+      "app development",
+      "web development",
+      "game development",
+      "data structures",
+      "programming",
+      "machine learning",
+    ],
+    required: false,
+  },
+  followers: {
+    type: Array,
+    required: false,
+  },
+});
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = bcrypt.hash(password, salt);
+  return hash;
+};
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ app.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const User = require("./Models/User");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(cors());
+
+require("./passport");
+
+const port = 3009;
+
+genToken = (user) => {
+  return jwt.sign(
+    {
+      iss: "Darshan",
+      sub: user.id,
+      iat: new Date().getTime(),
+      exp: new Date().setDate(new Date().getDate() + 1),
+    },
+    process.env.SECRET
+  );
+};
+
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv/config");
+}
+
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+// ROUTES
+
+app.post("/register", async function (req, res) {
+  const { name, email, password } = req.body;
+
+  //Check If User Exists
+  let foundUser = await User.findOne({ email });
+  if (foundUser) {
+    return res.status(403).json({ error: "Email is already in use" });
+  }
+
+  const newUser = new User({ name, email, password });
+  //   encrypt password
+  newUser.password = await newUser.encryptPassword(password);
+
+  await newUser.save();
+  // Generate JWT token
+  const token = genToken(newUser);
+  res.status(200).json({ token, user: newUser });
+});
+
+app.post("/login", async function (req, res) {
+  const { email, password } = req.body;
+
+  //Check If User Exists
+  let foundUser = await User.findOne({ email });
+  if (!foundUser) {
+    return res.status(403).json({ error: "Invalid Credentials" });
+  }
+
+  // Check if password is correct
+  const isMatch = await foundUser.isValidPassword(password);
+  if (!isMatch) {
+    return res.status(403).json({ error: "Invalid Credentials" });
+  }
+
+  // Generate JWT token
+  const token = genToken(foundUser);
+  res.status(200).json({ token, user: foundUser });
+});
+
+// change password route
+app.post("/changePassword", async function (req, res) {
+  try {
+    const { email, password, newPassword } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user) {
+      const isMatch = await user.isValidPassword(password);
+      if (!isMatch) {
+        return res.status(403).json({ error: "Invalid Credentials" });
+      }
+      user.password = await user.encryptPassword(newPassword);
+      await user.save();
+      const token = genToken(user);
+      res
+        .status(200)
+        .json({ token, user: user, message: "Password changed successfully" });
+    }
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+});
+
+app.get(
+  "/secret",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    res.json("Secret Data");
+  }
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
